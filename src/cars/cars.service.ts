a3ecd2707aa255e8df786a06c4d80578c43ef781
Luis Carlos <luis65@misena.edu.co>
2023-05-21 18:10:43 -0500

    Inyección de dependencias

---
+++ src/cars/cars.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class CarsService {
+  private cars = [
+    {
+      id: 1,
+      brand: 'Toyota',
+      model: 'Corolla',
+    },
+    {
+      id: 2,
+      brand: 'Honda',
+      model: 'Civic',
+    },
+    {
+      id: 3,
+      brand: 'Jeep',
+      model: 'Cherokee',
+    },
+  ];
+  findAll() {
+    return this.cars;
+  }
+  findOneById(id: number) {
+    return this.cars.find((element) => element.id === id);
+  }
+}
